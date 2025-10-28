79d2cce3e4b426072b5abb99d8d03ef7d3ae8ccc
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2025-10-28 09:15:29 +0300

    fix(ts): use namespace React imports and add federated module typings to avoid esModuleInterop requirement

---
+++ apps/host/src/types/federation.d.ts
@@ -0,0 +1,11 @@
+declare module 'chat/App' {
+  import * as React from 'react'
+  const Comp: React.ComponentType
+  export default Comp
+}
+
+declare module 'email/App' {
+  import * as React from 'react'
+  const Comp: React.ComponentType
+  export default Comp
+}
